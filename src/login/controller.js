af16877d0a4efefe03898aea16613548de754a55
Delaney <djyeakl@pointpark.edu>
2025-02-13 11:27:00 -0500

    changes made in class - still need updates

--- src/login/controller.js
+++ src/login/controller.js
@@ -8,8 +8,46 @@ const getLogin = (req, res) => {
     });
 };
 
+const getLoginById = (req, res) => {
+    const accountID = parseInt(req.params.id);
+    if (isNaN(accountID)) {
+        return res.status(400).json({error: "Invalid value format"});
+    }
+    console.log("Received AccountID");
+    pool.query(queries.getLoginById, [accountID], (error, results) => {
+        if(error){
+            console.error("Database Error", error);
+            return res.status(500).json({error: "Account Not Found"});
+        }
+        res.status(200).json(results.rows);
+    });
+}
+
+const addNewLogin = (req, res) => {
+    const {username,password} = req.body;
+
+    pool.query(queries.addNewLogin, [username,password],(error, results) => {
+        if(error)throw error;
+        res.status(200).json(results.rows);
+        res.status(201).send("Successfully added a new account");
+    });
+};
+
+const updateLogin = (req, res) => {
+    const {username,password} = req.body;
+
+    pool.query(queries.updateLogin, [username,password],(error, results) => {
+        if(error)throw error;
+        res.status(200).json(results.rows);
+        res.status(201).send("Successfully updated an account");
+    });
+};
+
 module.exports = {
     getLogin,
+    getLoginById,
+    addNewLogin,
+    updateLogin,
 };
 
 pool.connect((error, client, release) => {
--- src/login/routes.js
+++ src/login/routes.js
@@ -3,5 +3,8 @@ const controller = require('./controller');
 
 const router = Router();
 router.get('/', controller.getLogin);
+router.get('/:id', controller.getLoginById);
+router.post('/', controller.addNewLogin);
+router.put('/:id', controller.updateLogin);
 
 module.exports = router;
