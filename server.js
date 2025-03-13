18b4baf491870c2c8f7052da1d7982a52579068b
Delaney <djyeakl@pointpark.edu>
2025-03-13 13:49:39 -0400

    enabled cors for frontend

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require("express");
-const cors = require("cors");
+const cors = require('cors');
+const express = require('express');
+const app = express();
 
 //Routes
 const loginRoutes = require("./src/login/routes");
 
-const app = express();
 const port = 8008;
 
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
 
 //route
 app.get("/", (req, res) => {
