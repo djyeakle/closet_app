a0f57032e42b42d3e7552bf0b75d9e79f8b3597d
Delaney <djyeakl@pointpark.edu>
2025-03-19 15:50:13 -0400

    work?

--- src/login/queries.js
+++ src/login/queries.js
@@ -2,12 +2,10 @@ const getLogin = "select * from login";
 const getLoginById = 'select * from login where "accountID" = $1';
 const addNewLogin = "insert into login(username, password) values($1, $2)";
 const updateLogin = 'update login set username = $1, password = $2 where "accountID" = $3';
-const getClothes = "select * from clothes";
 
 module.exports = {
     getLogin,
     getLoginById,
     addNewLogin,
     updateLogin,
-    getClothes,
 };
