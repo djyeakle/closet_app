3ee0c6d6c38af2907bf9c9ca33efb96dd26cbae3
Delaney <djyeakl@pointpark.edu>
2025-04-07 18:23:16 -0400

    still fixing api

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     //user: "postgres",
     //password: "vbnmrno4",
     //host: "localhost",
-    host: "dpg-cupqojpopnds73efuuso-a",
+    host: "dpg-cupqojpopnds73efuus0-a.ohio-postgres.render.com",
     database: "closet_app",
     user: "djyeakle",
     password: "Sr4H9G5xicbGx8Qz6kLTokJEFFszGSkx",
